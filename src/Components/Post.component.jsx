065674ceb9eabf0e49c465026802168d976da111
tariqabusada <tariqesa101@gmail.com>
2022-09-21 20:43:10 +0200

    build: post components in front-end without style yet #3

--- src/Components/Post.component.jsx
+++ src/Components/Post.component.jsx
@@ -1,1 +1,18 @@
 import "./css/post.css";
+import React from "react";
+
+class Post extends React.Component {
+
+    
+    render() {
+        const { id, title, content, user_id } = this.props.onePost
+        return (
+            <>
+                <h2>{title}</h2>
+                <p>{content}</p>
+            </>
+        )
+    }
+}
+
+export default Post;
--- src/Components/PostList.component.jsx
+++ src/Components/PostList.component.jsx
@@ -1,1 +1,51 @@
 import "./css/postList.css";
+import React from "react";
+import Post from "./Post.component";
+
+class PostList extends React.Component {
+    state = {
+        userId: 0,
+        postslist: [],
+    };
+
+    componentDidMount() {
+        fetch("https://reddit-saeed.herokuapp.com/api/v1/posts/")
+            .then((res) => res.json())
+            .then((data) => {
+                this.setState({ postslist: data })
+                console.log('these are all posts \n', data)
+            });
+    };
+    componentDidUpdate() {
+        fetch(`https://reddit-saeed.herokuapp.com/api/v1/posts/user/${this.state.userId}`)
+            .then((res) => res.json())
+            .then((result) => this.setState({ postslist: result }
+            ));
+    };
+    increseId = () =>
+        this.setState((old) => {
+            return { userId: old.userId + 1 }
+        });
+    render() {
+        if (!this.state.postslist) return (<h2>Loading ... </h2>)
+        /* if (this.state.postslist === []) return (<h2>No post for user {this.state.userId}</h2>) */
+        return (
+            <>
+                <button onClick={this.increseId}>
+                    search on user {`${this.state.userId}`}
+                </button>
+                <ul className="postslist">
+                    {
+                        this.state.postslist.map((e) =>
+                            <li className="post"
+                                key={`${e.user_id}_${e.id}`}>
+                                <Post onePost={e} />
+                            </li>
+                        )
+                    }
+                </ul>
+            </>
+        )
+    }
+}
+export default PostList;
