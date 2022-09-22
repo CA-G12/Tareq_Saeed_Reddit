b36d80b822b1c790fbbbb2624410ae0623e5fe76
Saeed99Madi <saed.zooro@gmail.com>
2022-09-22 10:12:39 +0300

    build header and user list componets related to #4

--- src/Components/ContentSection.component.jsx
+++ src/Components/ContentSection.component.jsx
@@ -1,1 +1,33 @@
+import { Component } from "react";
 import "./css/contentSection.css";
+import UserList from "./UserList.component";
+class ContentSection extends Component {
+  state = { usersList: [], selectedUser: 0 };
+  changeSelectedUser = (data) => {
+    this.setState({
+      selectedUser: data,
+    });
+  };
+  componentDidMount() {
+    fetch("https://reddit-saeed.herokuapp.com/api/v1/users")
+      .then((data) => data.json())
+      .then((data) =>
+        this.setState({
+          usersList: data,
+        })
+      );
+  }
+
+  render() {
+    return (
+      <main>
+        <UserList
+          selectedUser={this.state.selectedUser}
+          setUserState={this.changeSelectedUser}
+          usersList={this.state.usersList}
+        />
+      </main>
+    );
+  }
+}
+export default ContentSection;
--- src/Components/UserList.component.jsx
+++ src/Components/UserList.component.jsx
@@ -1,1 +1,27 @@
 import "./css/userList.css";
+import { Component } from "react";
+import "./css/header.css";
+
+class UserList extends Component {
+  changeUserFilter = (event) => {
+    console.log(event.target.id);
+    const userId = event.target.id;
+    this.props.setUserState(userId);
+  };
+
+  render() {
+    const usersList = this.props.usersList;
+    return (
+      <section>
+        <ul>
+          {usersList.map((user) => (
+            <li onClick={this.changeUserFilter} id={user.id} key={user.id}>
+              {user.username}
+            </li>
+          ))}
+        </ul>
+      </section>
+    );
+  }
+}
+export default UserList;
