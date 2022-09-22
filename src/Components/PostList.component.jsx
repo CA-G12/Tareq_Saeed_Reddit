b5e981d0587704076ad02e4995965767b93f3858
Saeed99Madi <saed.zooro@gmail.com>
2022-09-22 18:49:26 +0300

    improve css related to #15

--- src/Components/PostList.component.jsx
+++ src/Components/PostList.component.jsx
@@ -3,24 +3,26 @@ import React from "react";
 import Post from "./Post.component";
 
 class PostList extends React.Component {
-
-    render() {
-        if (!this.props.posts) return (<h2>Loading ... </h2>)
-        
-        return (
-            <>
-                <div className="postslist">
-                    {
-                        this.props.posts.map((e) =>
-                            <div className="post"
-                                key={e.id}>
-                                <Post onePost={e} />
-                            </div>
-                        )
-                    }
-                </div>
-            </>
-        )
+  render() {
+    if (this.props.posts.length === 0)
+      return (
+        <div className='postslist'>
+          <div className='post'>no posts for this user</div>
+        </div>
+      );
+    else {
+      return (
+        <>
+          <div className='postslist'>
+            {this.props.posts.map((e) => (
+              <div className='post' key={e.id}>
+                <Post onePost={e} />
+              </div>
+            ))}
+          </div>
+        </>
+      );
     }
+  }
 }
 export default PostList;
